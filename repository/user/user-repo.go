baf166108f590a36e523abbcfb0a7f274af02cc8
jimmihu <jimmihuszz@gmail.com>
2022-03-23 17:11:23 +0700

    create user

--- repository/user/user-repo.go
+++ repository/user/user-repo.go
@@ -2,31 +2,21 @@ package repository
 
 import (
 	"latihanFSE/models/entity"
+	"latihanFSE/utils"
 
 	"github.com/jinzhu/gorm"
-	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
-
 func (repo *UserRepo) CreateUser(user *entity.User) *gorm.DB {
-	user.Password, _ = HashPassword(user.Password)
+	user.Password, _ = utils.HashPassword(user.Password)
 
-	role := getRoleViewer(repo)
+	role := getViewerRole(repo)
 	user.RoleID = role.ID
 	result := repo.mysqlConn.Create(&user)
 	return result
 }
 
-func getRoleViewer(repo *UserRepo) entity.Role {
+func getViewerRole(repo *UserRepo) entity.Role {
 
 	role := entity.Role{}
 	repo.mysqlConn.Where("title = ?", "viewer").First(&role)
