d92893fc09f07d13df88d9edf5db7870bedad18f
jimmihu <jimmihuszz@gmail.com>
2022-03-31 15:03:30 +0700

    s

--- config/configuration.go
+++ config/configuration.go
@@ -21,7 +21,7 @@ var env = map[string]map[string]string{
 
 		"PORT": "8000",
 
-		"MYSQL_HOST":   "localhost",
+		"MYSQL_HOST":   "127.0.0.1",
 		"MYSQL_PORT":   "3306",
 		"MYSQL_USER":   "root",
 		"MYSQL_PASS":   "",
